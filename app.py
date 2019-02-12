ecb49cbb47f800da2804546c8d64a2bba6e30356
Pablo Guardado <pabloguardadoalvarez@gmail.com>
2019-02-12 12:26:42 +0100

    cosas añadidas

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask ,jsonify
+import requests
+
+req = requests.get()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify({"nombre":"pedro"},{"apellido":"fernandez"})
 
 
 if __name__ == '__main__':
